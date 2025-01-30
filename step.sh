b6e7c3ea8b37d9f93ff71d4098689b4ecdb4d641
Mir Rakin Ishmam <mir.ishmam@hh0fw622pd.ad.deliveryhero.com>
2025-01-30 15:25:23 +0100

    test 2

--- step.sh
+++ step.sh
@@ -1,5 +1,37 @@
 #!/bin/bash
 set -ex
 
+# generating sts token to access the test-data-hub api
+function generate_sts_token {
+  local STS_TA_KEY_ID="$1"
+  local STS_TA_CLIENT_ID="$2"
+  local STS_TA_PRIVATE_KEY="$3"
+  local STS_ISSUER="$4"
 
-envman add --key STS_TOKEN --value "STS_TOKEN_VALUE"
+  HEADER_RAW=$(printf '{"alg":"RS256","typ":"JWT","kid":"%s"}' "$STS_TA_KEY_ID")
+  PAYLOAD_RAW=$(printf '{"aud":"https://sts.deliveryhero.io","jti":"%s","exp":%d,"iss":"%s","sub":"%s"}' "$(uuidgen)" $(( $(date +%s) + 600 )) "$STS_TA_CLIENT_ID" "$STS_TA_CLIENT_ID")
+
+
+  HEADER=$(printf "%s" "${HEADER_RAW}" | openssl base64 -A | tr -d '=' | tr '/+' '_-' )
+  PAYLOAD=$(printf "%s" "${PAYLOAD_RAW}" | openssl base64 -A | tr -d '=' | tr '/+' '_-' )
+  HEADER_PAYLOAD="${HEADER}"."${PAYLOAD}"
+
+  key_file=$(mktemp)
+  printf "%s" "$STS_TA_PRIVATE_KEY" > "$key_file"
+
+  SIGNATURE=$(printf "%s" "${HEADER_PAYLOAD}" | openssl dgst -sha256 -sign "$key_file" | openssl base64 -A | tr -d '=' | tr '/+' '_-' )
+  CLIENT_ASSERTION="${HEADER_PAYLOAD}"."${SIGNATURE}"
+
+  response=$(curl --location --request POST "$STS_ISSUER" \
+  --header "Content-Type: application/x-www-form-urlencoded" \
+  --data-urlencode "grant_type=client_credentials" \
+  --data-urlencode "client_assertion_type=urn:ietf:params:oauth:client-assertion-type:jwt-bearer" \
+  --data-urlencode "client_assertion=$CLIENT_ASSERTION")
+
+  access_token=$(echo "$response" | grep -o '"access_token":"[^"]*' | grep -o '[^"]*$')
+  envman add --key STS_TOKEN --value "$access_token"
+}
+
+generate_sts_token "$STS_TA_KEY_ID" "$STS_TA_CLIENT_ID" "$STS_TA_PRIVATE_KEY" "$STS_ISSUER"
+
+
